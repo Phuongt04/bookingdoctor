ffe68b786bb1dddb1e52d92d1e33155d4cd6bef4
Phuongt04 <ttphuong10404@gmail.com>
2026-01-05 00:33:45 +0700

    fix admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -118,14 +118,14 @@ const aminLogin = async (req, res) => {
       error: error.message,
     });
   }
-  const allDoctors = async (req, res) => {
-      try {
-          const doctors = await doctorModel.find({})
-          res.json({ success: true, doctors })
-      } catch (error) {
-          console.log(error)
-          res.json({ success: false, message: error.message })
-      }
-  }
+const allDoctors = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({})
+        res.json({ success: true, doctors })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 };
 export { addDoctor, aminLogin, allDoctors };
