5fdc787569c265976d261e7e159be894aa7b5edc
Phuongt04 <ttphuong10404@gmail.com>
2026-01-05 02:56:02 +0700

    hehe

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addDoctors, adminLogin } from "../controllers/adminController.js"; // Đảm bảo đúng tên hàm
+import { addDoctors, aminLogin } from "../controllers/adminController.js"; // Đảm bảo đúng tên hàm
 import { authAdmin } from "../middleware/authAdmin.js";
 import upload from "../middleware/multer.js"; // <-- QUAN TRỌNG: Phải có dòng này
 
 const adminRouter = express.Router();
 
-adminRouter.post("/login", adminLogin);
+adminRouter.post("/login", aminLogin);
 
 // Dòng này BẮT BUỘC phải có 'upload.single("image")' thì mới gửi ảnh được
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctors);
