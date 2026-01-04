3b25bde50359dada7ed3eda99e2cacb9abdf8198
Phuongt04 <ttphuong10404@gmail.com>
2026-01-04 15:34:50 +0700

    Fix loi duong dan assets

--- ADMIN/src/pages/Login.jsx
+++ ADMIN/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { assets } from "./../assets/assets_admin/assets";
+import { assets } from "./../assets/assets_admin/assets.js";
 import { Admincontext } from "../context/AdminContext";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
