38662b077a6111dffbd4283b66b49bd3d07d0186
Phuongt04 <ttphuong10404@gmail.com>
2026-01-05 00:50:24 +0700

    on

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addDoctor, aminLogin, allDoctors } from "../controllers/adminController.js";
+import {aminLogin, addDoctors } from "../controllers/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
 
 const adminRouter = express.Router();
 // Route to add a doctor
-adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
+adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctors);
 adminRouter.post("/login", aminLogin);
 adminRouter.post('/all-doctors', authAdmin, allDoctors) // Thêm dòng này
 export default adminRouter;
